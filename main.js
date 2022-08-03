410d1b48adf4fc0a1630286d15e3216410453044
shafea87 <m.s.alsalkini@gmail.com>
2022-08-03 13:19:05 +0200

    media query

---
+++ main.js
@@ -0,0 +1,9 @@
+// Execute script after page load
+window.onload = function digital_fn() {
+  let toggle = document.querySelector('.nav-toggle');
+  let collapse = document.querySelector('.nav-collapse');
+
+  toggle.addEventListener('click', function (e) {
+    collapse.classList.toggle('active'); 
+  });
+};
